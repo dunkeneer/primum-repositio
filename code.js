09bd335d101d73565b6078841e810f45dae00622
dunkeneer <122392711+dunkeneer@users.noreply.github.com>
2023-02-16 17:05:01 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,9 +1,47 @@
+//empty function; code will be added later
+function startButtonClick(){}
+
+//empty function; code will be added later
+function stopButtonClick(){}
+
+//this function will ask for a first name, last name and badge number
+//the names need to be less than 20 characters and the badge number needs to be three characters or less
+function getUserInput(){
+    var fullName = "";
+    var badgeNumber = 0;
+
+    do{
+        //the full name
+        var firstName = prompt("Please enter your first name: ");
+        var lastName = prompt("please enter your last name: ");
+
+        fullName = firstName + " " + lastName;
+        
+        if (fullName.length > 20){
+            alert("Please enter a shorter name! Length was: " + fullName.length);
+        }
+    }
+    while(fullName.length > 20);
+
+        do{
+    //the badge number
+    badgeNumber = prompt("Please enter your badge number: ")
+    
+    if (badgeNumber > 999){
+        alert("Please enter a shorter badge number!")
+        }
+    }while (badgeNumber > 999);
+
+    return fullName + ", Badge number " + badgeNumber;
+}
+
+//this is our new countdown function
 function runTimer(countdownElem){
      //tracks the current countdown time
      var currTime = 50;
 
      //tracks the current time
-     var timeout = 0;
+     var timeout = 3;
 
      //this value will not change and creates the five second delay
      var timeoutIncrement = 1000;
