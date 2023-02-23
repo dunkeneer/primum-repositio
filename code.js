597ffb45bfb88c56a918faf769263a45e8e60b15
dunkeneer <122392711+dunkeneer@users.noreply.github.com>
2023-02-23 11:19:52 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,8 +1,25 @@
 //empty function; code will be added later
-function startButtonClick(){}
+function startButtonClick(){
+    //don't let the user click the start button while countdown is running
+    document.getElementById("btnStart").disabled = true;
+    document.getElementById("btnStop").disabled = false;
+
+    //shorthand to point to the
+    var countdownElem = document.getElementById("countdown");
+    runTimer(countdownElem);
+}
+
+var arrInterval = new Array();
 
 //empty function; code will be added later
-function stopButtonClick(){}
+function stopButtonClick(){
+    document.getElementById("btnStart").disabled = false;
+    document.getElementById("btnStop").disabled = true;
+
+    for (counter = 0; counter <11; counter++){
+        clearTimeout(arrInterval[counter]);
+    }
+}
 
 //this function will ask for a first name, last name and badge number
 //the names need to be less than 20 characters and the badge number needs to be three characters or less
@@ -43,25 +60,25 @@ function runTimer(countdownElem){
      //tracks the current time
      var timeout = 3;
 
-     //this value will not change and creates the five second delay
+     //this value will not change and creates the one second delay
      var timeoutIncrement = 1000;
 
      //loops
-     for(var counter=0; counter < 51; counter++){
+     for(var counter=0; counter < 11; counter++){
          //comment this one
-        setTimeout(function(){
+        arrInterval[counter] = setTimeout(function(){
             if(currTime == 0){
             alert("Blastoff!");
             countdownElem.innerHTML = "Launch successful";
         } //half-way stretch
             else if (currTime <25){
-            countdownElem.innerHTML = "Nothing less than 1/2 to launch, time left = " + currTime;
+            countdownElem.innerHTML = "Nothing less than 1/2 to launch, time left: = " + currTime;
         }
         else{
             countdownElem.innerHTML = currTime;
         }
 
-        currTime = currTime - 1;
+        currTime = currTime - 5;
     }, timeout);
     timeout = timeout + timeoutIncrement;
      }
